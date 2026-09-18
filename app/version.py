70bab4f0929fd22510a1ebe47aae7bf97898ec27
dynaconsurv <fuadpuek@gmail.com>
2026-09-18 15:14:16 +0800

    chore(release): bump version to 1.1.5

--- app/version.py
+++ app/version.py
@@ -1,6 +1,6 @@
 """
 Application version and repository metadata.
 """
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 APP_NAME = "AVEVA PI to Oracle ERP Cloud Data Pipeline"
 GITHUB_REPO = "dynaconsurv/pidatapipeline"
